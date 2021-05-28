9e5e2fc0458d981abe049b54c34d1c096a61dcf5
Luis Alberto Rodríguez <38012889+luisrr1590@users.noreply.github.com>
2021-05-27 21:29:50 -0500

    Add files via upload

---
+++ Assets/Rotate.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    // Use this for initialization
+    public float ySpeed = 100.0f;
+    public float xSpeed = 100.0f;
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        transform.Rotate(0, ySpeed * Time.deltaTime,0);
+        if (Input.GetMouseButton(0))
+            {
+            float h = xSpeed * Input.GetAxis("Mouse X");
+            float v = ySpeed * Input.GetAxis("Mouse Y");
+            transform.Rotate(-v, -h, 0);
+            }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            ySpeed += 10.0f;
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            ySpeed -= 10.0f;
+        }
+        
+    }
+
+}
